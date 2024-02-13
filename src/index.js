946ab7e1f40f7b0cce632512d1328106a6bcd1cb
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2024-02-13 22:44:39 +0530

    Setup Database Connection

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require("dotenv").config({ path: "./env" });
+
+/*import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+const app = express();*/
+import dotenv from "dotenv";
+import connectDB from "./db";
+
+dotenv.config({
+  path: "./env",
+});
+connectDB();
+
+/*
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", () => {
+      console.log("Error: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+    throw err;
+  }
+})();
+*/
