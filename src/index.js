b299e0bc6492de3c60bac0b329459f526a41cbd0
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2024-02-18 19:04:41 +0530

    api error and response handling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 // require("dotenv").config({ path: "./env" });
 
 /*import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import { DB_NAME } from "./constants";*/
 import express from "express";
-const app = express();*/
+const app = express();
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error");
+  });
 
 /*
 (async () => {
