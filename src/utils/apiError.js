65937ddf4e8556b9fbed942b50e3edf651a39392
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2024-02-18 19:47:28 +0530

    Api Response and Error handling

---
+++ src/utils/apiError.js
@@ -0,0 +1,23 @@
+class apiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something Went Wrong",
+    error = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = error;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { apiError };
