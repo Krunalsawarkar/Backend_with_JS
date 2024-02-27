5ed1997b71753ff2c36d8fe5d0425cfe4fa1973d
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2024-02-27 22:11:17 +0530

    Adding controller logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,52 +1,53 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError, apiError } from "../utils/apiError.js";
+import { apiError } from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { apiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  //get user details from frontend
-  //validation - not empty
+  // get user details from frontend
+  // validation - not empty
   // check if user already exists: username, email
   // check for images, check for avatar
-  // upload them to cloudinary , avatar
+  // upload them to cloudinary, avatar
   // create user object - create entry in db
   // remove password and refresh token field from response
-  //check for user creation
-  //return res
+  // check for user creation
+  // return res
 
-  const { fullName, email, username, password } = req.body;
+  const { fullname, email, username, password } = req.body;
+  //console.log("email: ", email); 
 
   if (
-    [fullName, email, username, password].some((field) => field?.trim() === "")
+    [fullname, email, username, password].some((field) => field?.trim() === "")
   ) {
-    throw new apiError(400, "All Fields are Required");
+    throw new apiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
   if (existedUser) {
-    throw new apiError(409, "User with email and username already exists");
+    throw new apiError(409, "User with email or username already exists");
   }
+  console.log(req.files);
 
-  const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  const avatarLocalPath = req.files?.["avatar"]?.[0]?.path;
+  const coverImageLocalPath = req.files?.["coverImage"]?.[0]?.path;
 
   if (!avatarLocalPath) {
-    throw new apiError(400, "Avatar File is required");
+    throw new apiError(400, "Avatar file is required");
   }
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-
   if (!avatar) {
-    throw new apiError(400, "Avatar File is Required");
+    throw new apiError(400, "Avatar file is required");
   }
 
-  const userCreate = await User.create({
-    fullName,
+  const user = await User.create({
+    fullname,
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
     email,
@@ -54,17 +55,17 @@ const registerUser = asyncHandler(async (req, res) => {
     username: username.toLowerCase(),
   });
 
-  const createdUser = await User.findById(userCreate._id).select(
+  const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
 
   if (!createdUser) {
-    throw new apiError(500, "Something Went Wrong While Registering User.");
+    throw new apiError(500, "Something went wrong while registering the user");
   }
 
   return res
     .status(201)
-    .json(new apiResponse(200, createdUser, "User Registered Sucessfully"));
+    .json(new apiResponse(200, createdUser, "User registered Successfully"));
 });
 
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -32,7 +32,6 @@ const userSchema = new Schema(
     },
     coverImage: {
       type: String,
-      required: true,
     },
     watchHistory: [
       {
@@ -40,12 +39,12 @@ const userSchema = new Schema(
         ref: "Video",
       },
     ],
-    passsword: {
+    password: {
       type: String,
       required: [true, "Password is required"],
     },
     refreshToken: {
-      type: string,
+      type: String,
     },
   },
   { timestamps: true }
@@ -53,12 +52,12 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.passsword = await bcrypt.hash(this.passsword, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.passsword);
+  return await bcrypt.compare(password, this.password);
 };
 
 userSchema.methods.generateAccessToken = function () {
