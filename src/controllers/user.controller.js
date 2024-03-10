02c73e46a104b717f555312752785ee41ecedfc0
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2024-03-10 21:13:16 +0530

    Sub Aggregated Pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -372,6 +372,59 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "video",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "user",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(
+      new apiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch History Fetched Successfully!!"
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -383,4 +436,5 @@ export {
   updateUserAvatar,
   updateUserCoverImg,
   getUserChannelProfile,
+  getWatchHistory,
 };
